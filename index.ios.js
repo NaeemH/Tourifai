693aafc53197344cb7d190805ba1c06b81c0827f
Nicola Paolucci <npaolucci@atlassian.com>
2017-01-17 14:21:55 -0500

    Trouble with API keys and current version of XCode

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,7 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Naeem Hossain
  */
 
-import Clarifai from 'clarifai';
 import React, { Component } from 'react';
 import ImagePicker from 'react-native-image-picker';
 import {
@@ -15,11 +12,6 @@ import {
   TouchableHighlight,
   Image
 } from 'react-native';
-
-var app = new Clarifai.App(
-  'izJI69QTq7yCztqd-nG3heI18gvdHlP828AQPPig',
-  'kKqDdGFh_l-kZMuicRi-EWxVYxYGZ8p6Q1a4x8A5'
-);
 
 var options = {
   title: 'Select an Image',
@@ -32,7 +24,7 @@ var options = {
 export default class Tourifai extends Component {
   constructor() {
     super();
-    this.state = {imageSource:'https://community.clarifai.com/uploads/default/_emoji/clarifai.png', tagText: ''};
+    this.state = {imageSource:'https://community.clarifai.com/uploads/default/_emoji/clarifai.png'};
   }
   selectImage(){
   ImagePicker.showImagePicker(options, (response) => {
@@ -46,18 +38,6 @@ export default class Tourifai extends Component {
     else {
       // Do something with the selected image
       this.setState({imageSource: response.uri.replace('file://', '')});
-      app.models.predict(Clarifai.GENERAL_MODEL, {base64:response.data}).then(
-      (res) => {
-        console.log('Clarifai response = ', res);
-        let tags = '';
-        for (let i = 0; i<res.data.outputs[0].data.concepts.length; i++) {
-          tags += res.data.outputs[0].data.concepts[i].name + ' ';
-        }
-        this.setState({tagText:tags});
-      },
-      (error)=>{
-        console.log(error);  
-      });
     }
   });
 }
@@ -71,7 +51,6 @@ export default class Tourifai extends Component {
           source={{uri: this.state.imageSource}}
           style={styles.image}
           />
-          <Text>{this.state.tagText}</Text>
       </View>
     );
   }
